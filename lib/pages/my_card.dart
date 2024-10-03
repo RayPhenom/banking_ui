4a967b9b97af2113e17504ee8c745019e74031de
RayPhenom <lumuonix@gmail.com>
2024-10-03 16:19:43 +0300

    front and back of card

--- lib/pages/my_card.dart
+++ lib/pages/my_card.dart
@@ -22,21 +22,150 @@ class MyCardPage extends StatelessWidget {
           style: TextStyle(fontWeight: FontWeight.w500),
         ),
       ),
-      body: const SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.all(22.0),
+          padding: const EdgeInsets.all(22.0),
           child: Column(
             children: [
-              SizedBox(
+              const SizedBox(
                 height: 20,
               ),
-              CreditCard(),
-              SizedBox(
+              const FrontCard(),
+              const SizedBox(
                 height: 25,
               ),
-              CreditCard(),
+              const CreditCard(),
+              TextButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.add),
+                label: const Text(
+                  "Add Card",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  side: const BorderSide(color: Colors.grey),
+                  fixedSize: const Size(double.maxFinite, 60),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(14),
+                  ),
+                  backgroundColor: Colors.grey,
+                  foregroundColor: Colors.black,
+                ),
+              )
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class FrontCard extends StatelessWidget {
+  const FrontCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 240,
+      decoration: BoxDecoration(borderRadius: BorderRadius.circular(20)),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 2,
+              child: Container(
+                color: const Color.fromARGB(255, 14, 19, 29),
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: 16,
+                      left: 16,
+                      child: Image.asset(
+                        "assets/credit-card.png",
+                        height: 40,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Positioned(
+                      top: 20,
+                      left: 70,
+                      child: Image.asset(
+                        "assets/wifi.png",
+                        height: 40,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const Positioned(
+                      bottom: 16,
+                      left: 16,
+                      child: Text(
+                        "**** **** **** 6407",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Container(
+                color: const Color.fromARGB(255, 16, 80, 98),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Onix Lumumba",
+                            style: TextStyle(
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Text(
+                            "10/26",
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          CircleAvatar(
+                            radius: 15,
+                            backgroundColor: Colors.white.withOpacity(0.9),
+                          ),
+                          Transform.translate(
+                            offset: const Offset(-10, 0),
+                            child: CircleAvatar(
+                              radius: 15,
+                              backgroundColor: Colors.white.withOpacity(0.9),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
       ),
     );
@@ -67,8 +196,7 @@ class BackCard extends StatelessWidget {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.all(
-                16.0),
+            padding: const EdgeInsets.all(16.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
