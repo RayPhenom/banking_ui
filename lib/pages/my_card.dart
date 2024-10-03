ce50e3479d01d9414a40b2c79160c592ccce16d6
RayPhenom <lumuonix@gmail.com>
2024-10-03 14:24:30 +0300

    Card details up

--- lib/pages/my_card.dart
+++ lib/pages/my_card.dart
@@ -1,3 +1,4 @@
+import 'package:banking_ui/widgets/credit_card.dart';
 import 'package:flutter/material.dart';
 
 class MyCardPage extends StatelessWidget {
@@ -5,10 +6,124 @@ class MyCardPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Display My page"),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: IconButton.outlined(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.arrow_back_ios_new,
+            size: 20,
+          ),
+        ),
+        title: const Text(
+          "My Credit Card",
+          style: TextStyle(fontWeight: FontWeight.w500),
+        ),
+      ),
+      body: const SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(22.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 20,
+              ),
+              CreditCard(),
+              SizedBox(
+                height: 25,
+              ),
+              CreditCard(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 
+class BackCard extends StatelessWidget {
+  const BackCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 240,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: const Color.fromARGB(255, 15, 20, 30),
+      ),
+      child: Stack(
+        children: [
+          Positioned(
+            right: 0,
+            top: 0,
+            bottom: 0,
+            child: Image.asset(
+              "assets/card-design.png",
+              fit: BoxFit.cover,
+              width: 160,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(
+                16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        CircleAvatar(
+                          radius: 15,
+                          backgroundColor: Colors.white.withOpacity(0.8),
+                        ),
+                        Transform.translate(
+                          offset: const Offset(-10, 0),
+                          child: CircleAvatar(
+                            radius: 15,
+                            backgroundColor: Colors.white.withOpacity(0.8),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                const Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "**** **** **** 6407",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                      ),
+                    ),
+                    Text(
+                      "10/26",
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+                const Text(
+                  "Onix Amimo Lumumba",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
